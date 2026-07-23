c20201f67ff8efb408478049e2e1c60a0c8f7d3e
Maheswara VULAVALA <maheshvulavala62@gmail.com>
2026-07-23 10:45:13 +0000

    e

--- models/test_mt.sql
+++ models/test_mt.sql
@@ -6,5 +6,6 @@ role as employee_role,
 location,
 case when location = 'HYD' then 'GHMC' else '' end as loc_code,
 phone_number as contact
-from  ANALYTICS.DATAENGINEER_TEST.MOURITECH
+-- from  ANALYTICS.DATAENGINEER_TEST.MOURITECH
+from {{ source('DATAENGINEER_TEST', 'MOURITECH') }}
 
